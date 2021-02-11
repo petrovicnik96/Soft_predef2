14bfcd3602c1e60b4d4289cda1858ed9412cb81f
Nikola Petrovic <petrovicnik96@gmail.com>
2021-02-11 20:10:38 +0100

    test

--- main.py
+++ main.py
@@ -11,12 +11,14 @@ img_height = 350
 X = []
 Y = []
 
-for i in tqdm(range(csvData.shape[0])):
-    path = 'metadata/' + csvData['file_name'][i]
-    img = image.load_img(path, target_size=(img_width, img_height, 3))
+for i in tqdm(range(csvData.shape[1])):
+    path = 'metadata/train/' + csvData['file_name'][i]
+    img = image.load_img(path, target_size=(img_width, img_height, 2))
     img = image.img_to_array(img)
     img = img / 255.0
     X.append(img)
 
 X = np.array(X)
-print(len(X))
+# print(len(X))
+y = csvData.drop(columns=['file_name', 'weather'])[0:csvData.shape[0]]
+y.head()
